f046166c1bc9be08583f9090e008ce37ef182831
taofiqur <taofiqurrohman11@gmail.com>
2023-05-04 09:07:51 +0700

    fragment

---
+++ MyFragment-master/README.md
@@ -0,0 +1,3 @@
+# MyFragment
+![Fragment1](https://user-images.githubusercontent.com/107418303/222304385-1d7b2688-e270-412b-a214-695586db4e48.jpeg)
+![Fragment2](https://user-images.githubusercontent.com/107418303/222304438-89ded84f-e0f2-48b7-ac2a-51c65beb073f.jpeg)
---
+++ MyFragment-master/app/src/main/java/com/example/myfragment/ViewPagerAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.myfragment
+
+import android.content.Context
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class ViewPagerAdapter (private val mContext: Context, fm:FragmentManager):
+    FragmentPagerAdapter(fm){
+    override fun getCount(): Int {
+        return 2
+    }
+
+    override fun getItem(position: Int): Fragment {
+        return if (position == 0){
+            fragment_first.newInstance()
+        }else{
+            fragment_second.newInstance()
+        }
+
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return mContext.resources.getString(TAB_TITLES[position])
+    }
+
+    companion object{
+        @StringRes
+        private val TAB_TITLES = intArrayOf(R.string.tab_text_1, R.string.tab_text_2)
+    }}
+
